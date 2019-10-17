b6c611619dd80ab3283a130bdd92b54703add021
Daniel YU <i@icymint.me>
2019-10-17 09:49:40 +0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Deploy $DOCKER_USERNAME/$NAME:$VERSION"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/$NAME:$VERSION
 
-if [ "$AS_DEF" -eq "true" ]; then
+if [ "$AS_DEF" = "true" ]; then
   docker tag $DOCKER_USERNAME/$NAME:$VERSION $DOCKER_USERNAME/$NAME:latest
   docker push $DOCKER_USERNAME/$NAME:latest
 fi
