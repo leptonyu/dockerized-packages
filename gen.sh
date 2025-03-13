7a40aea99bb8a3c9e28729193590ba5ce4d90221
Daniel Yu <i@icy.me>
2025-03-13 22:36:01 +0800

    Add enhanced fake dns

--- gen.sh
+++ gen.sh
@@ -4,12 +4,14 @@ DNS="tcp://8.8.8.8:53"
 #DNS="tcp://209.244.0.3:53"
 #DNS_CN="119.29.29.29"
 DNS_CN="tcp://223.5.5.5:53 119.29.29.29"
+DNS_FAKE="192.168.5.5"
 
 gen(){
 cat <<-EOF
 $DNS
 [/cluster.local/]10.96.0.10
 [/gacjie.cn/]$DNS
+[/github.com/]$DNS_FAKE
 EOF
 awk '-F[/]' -v dns="$DNS_CN" '{print "[/"$2"/]"dns}' \
   dnsmasq-china-list/accelerated-domains.china.conf \
