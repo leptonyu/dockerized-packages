25d08355f1810c70a9064b6db419491eff17dbf6
Daniel Yu <i@icy.me>
2025-03-14 10:06:11 +0800

    Default use local

--- gen.sh
+++ gen.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 DNS="192.168.5.5:5333"
-#DNS="tcp://209.244.0.3:53"
-#DNS_CN="119.29.29.29"
+DNS_US="8.8.8.8 8.8.4.4"
 DNS_CN="tcp://223.5.5.5:53 119.29.29.29"
-# DNS_FAKE="192.168.5.5:5333"
 
 gen(){
 cat <<-EOF
 $DNS
 [/cluster.local/]10.96.0.10
-[/gacjie.cn/]$DNS
+[/arpa/]DNS_US
 EOF
-# awk '-F[/]' -v dns="$DNS_FAKE" '{print "[/"$1"/]"dns}' domain.txt
 awk '-F[/]' -v dns="$DNS_CN" '{print "[/"$2"/]"dns}' \
   dnsmasq-china-list/accelerated-domains.china.conf \
   dnsmasq-china-list/google.china.conf \
